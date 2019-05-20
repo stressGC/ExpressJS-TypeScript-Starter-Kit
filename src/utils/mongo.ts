252a8376a4122ddcce140b0acb1f22123b65ab16
stressGC <g.cosson@live.fr>
2019-05-20 00:12:40 -0400

    cleaning after circleci work

--- src/utils/mongo.ts
+++ src/utils/mongo.ts
@@ -14,30 +14,19 @@ const options = {
  * initialise mongoDB connection
  */
 const init = () => {
-  console.log('starting mongo init');
   /* remove deprecated options */
   mongoose.set('useCreateIndex', true);
   mongoose.set('useFindAndModify', false);
 
-  const DB_NAME = process.env.NODE_ENV === 'test' ?
-    process.env.MONGO_TEST_DB
-    :
-    process.env.MONGO_DB;
-  console.log('MONGO_HOST', process.env.MONGO_HOST);
-  console.log('MONGO_PORT', process.env.MONGO_PORT);
-  console.log('DB_NAME', DB_NAME);
-  console.log(`mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${DB_NAME}`);
   /* connect */
   mongoose.connect(
-    `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${DB_NAME}`,
+    `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`,
     options,
     (err) => {
       if (err) {
-        console.log(`Error while connecting to MongoDB: ${err}`)
         winston.error(`Error while connecting to MongoDB: ${err}`);
         return process.exit(0);
       }
-      console.log("connected to mongodb")
       winston.debug('Connected to MongoDB');
 
     /* close mongo connection on SIGINT */
