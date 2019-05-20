bcdbb948ed75301dfd3d2e1f15be5f7b1e45bb6f
stressGC <g.cosson@live.fr>
2019-05-19 21:24:16 -0400

    update config

--- test/api/home.test.ts
+++ test/api/home.test.ts
@@ -4,7 +4,6 @@ import app from '../../src/app';
 import { expect } from 'chai';
 import * as request from 'supertest';
 import * as HTTPStatus from 'http-status-codes';
-import logger from './../../src/utils/logger/winston';
 
 describe('test global API behavior', () => {
   it('should return 404 if no path matched', (done) => {
@@ -12,9 +11,9 @@ describe('test global API behavior', () => {
     request(app)
       .get(randomPath)
       .end((err, res) => {
+        expect(err).to.be.a('string');
         expect(err).to.be.null;
         expect(res.status).to.equal(HTTPStatus.NOT_FOUND);
-        logger.error('HIHIHIHAHAHA', err, res);
         done();
       });
   });
